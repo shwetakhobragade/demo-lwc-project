20316248e48d9427082106c4865de46e28e4d674
shwetakhobragade <shwetakhobragade18@gmail.com>
2023-01-24 14:47:21 +0530

    hi

--- force-app/main/default/lwc/demosecondproject/demosecondproject.js
+++ force-app/main/default/lwc/demosecondproject/demosecondproject.js
@@ -1,3 +1,51 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire,api } from 'lwc';
+import { subscribe, unsubscribe, APPLICATION_SCOPE, MessageContext} from 'lightning/messageService';
+import searchMessage from '@salesforce/messageChannel/searchgit__c';
+const QUERY_USER_ENDPOINT_URL='https://api.github.com/search/users?q=';
+export default class demosecondproject extends LightningElement {
+    @api personName;
+    retrivedusers=[];
+    subscription = null;
 
-export default class Demosecondproject extends LightningElement {}
+    @wire(MessageContext)
+    messageContext;
+
+    connectedCallback() {
+        this.subscribeToMessageChannel();
+    }
+
+    disconnectedCallback() {
+        this.unsubscribeToMessageChannel();
+    }
+
+    // Encapsulate logic for Lightning message service subscribe and unsubsubscribe
+    subscribeToMessageChannel() {
+        if (!this.subscription) {
+            this.subscription = subscribe(
+                this.messageContext,
+                searchMessage,
+                (message) => this.handleMessage(message),
+                { scope: APPLICATION_SCOPE }
+            );
+        }
+    }
+
+    async handleMessage(message) {
+       console.log('handleMessage:', message);
+       this.personName=message.searchTerm;
+       let queryEndPoint=QUERY_USER_ENDPOINT_URL+this.personName;
+       try{
+        const RESPONSE=await fetch(queryEndPoint);
+        const USER_LIST=await RESPONSE.json();
+        console.log(USER_LIST.items);
+        this.retrivedusers=USER_LIST.items;
+       }catch(error){
+          console.log(error); 
+      }
+    }
+
+    unsubscribeToMessageChannel() {
+        unsubscribe(this.subscription);
+        this.subscription = null;
+    }
+}
---
+++ force-app/main/default/lwc/firstDemoComponent/firstDemoComponent.js
@@ -0,0 +1,18 @@
+import { LightningElement,track,wire} from 'lwc';
+import { publish, MessageContext } from 'lightning/messageService';
+import searchMessage from '@salesforce/messageChannel/searchgit__c';
+export default class FirstComponent extends LightningElement {
+    @track  searchText;
+    
+    @wire(MessageContext)
+    messageContext;
+
+    handleClick(event){
+    let inputvalue=this.template.querySelectorAll("lightning-input")[0].value;
+    const payload = { isTermvalid:true, searchTerm:inputvalue };
+
+        publish(this.messageContext, searchMessage, payload);
+    }
+    
+   
+}
