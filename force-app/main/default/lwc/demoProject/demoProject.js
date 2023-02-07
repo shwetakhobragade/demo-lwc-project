faf7856fa1776e400698ae80f4ac7a4d8bad0982
shwetakhobragade <shwetakhobragade18@gmail.com>
2023-02-07 11:59:29 +0530

    hi

--- force-app/main/default/lwc/demoProject/demoProject.js
+++ force-app/main/default/lwc/demoProject/demoProject.js
@@ -1,9 +1,3 @@
 import { LightningElement } from 'lwc';
 
-export default class DemoProject extends LightningElement {
-    handleClick(event){
-        console.time('gameengine time');
-        console.log('%c something!!! clicked for ','background: #222; color: #bada55; front-size
-        console.timeEnd('gameengine time');
-    }
-}
+export default class DemoProject extends LightningElement {}
